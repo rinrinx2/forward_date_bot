4383cceb89523f997fb08fd866d3e228285d9735
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2021-03-22 15:02:40 +0200

    first version

---
+++ bot.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const { Telegraf } = require('telegraf');
+const Extra = Telegraf.Extra;
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start(ctx =>
+  ctx.reply(`👋🏻 Welcome!\n
+  Forward me a message and I will tell you when it was sent 👀`)
+);
+
+bot.on('message', ctx => {
+  if (ctx.message.forward_date) {
+    const date = new Date(ctx.message.forward_date * 1000);
+    const day = date.getDate();
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const message_id = ctx.message.message_id;
+    ctx.reply(`${day} ${month} ${year} ${hours}:${minutes}:${seconds}`, {
+      reply_to_message_id: message_id,
+    });
+  } else {
+    ctx.reply('Pleeeease forward not send ☺️');
+  }
+});
+
+bot.launch();
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
