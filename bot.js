9209942041e3f29144716bd19a3a5a4d6135829a
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2021-03-25 21:04:10 +0200

    added tracking usage

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const axios = require('axios').default;
+
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -10,14 +12,20 @@ bot.start(ctx =>
 );
 
 bot.on('message', ctx => {
+  axios.post('https://api.countapi.xyz/hit/bogdanbryzh.me/forward_message_bot_sends')
+})
+
+bot.on('message', ctx => {
   if (ctx.message.forward_date) {
     const date = new Date(ctx.message.forward_date * 1000);
     const day = date.getDate();
     const month = date.toLocaleString('default', { month: 'long' });
     const year = date.getFullYear();
     const hours = date.getHours();
-    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-    const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    const minutes =
+      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    const seconds =
+      date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
     const message_id = ctx.message.message_id;
     ctx.reply(`${hours}:${minutes}:${seconds} UTC ${day} ${month} ${year}`, {
       reply_to_message_id: message_id,
